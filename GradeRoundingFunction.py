00838363e1a1b558e14ccc40fd8925c35e1007f3
PilsnerCoding <136451968+PilsnerCoding@users.noreply.github.com>
2023-06-14 10:31:48 +0200

    Add files via upload

---
+++ GradeRoundingFunction.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import math
+
+inp = np.random.uniform(-3, 12, 10)
+
+
+def roundGrade(grades):
+    i = 0
+    while i < len(grades):
+        if grades[i] >= 11:
+            grades[i]=12
+        elif grades[i] >= 8.5 and grades[i] < 11:
+            grades[i]=10
+        elif grades[i] >= 5.5 and grades[i] < 8.5:
+            grades[i]=7
+        elif grades[i] >= 3 and grades[i] < 5.5:
+            grades[i]=4
+        elif grades[i] >= 1 and grades[i] < 3:
+            grades[i]=2
+        elif grades[i] >= -1.5 and grades[i] < 1:
+            grades[i]=0
+        elif grades[i] < -1.5:
+            grades[i]=-3
+        i+=1
+    gradesRounded = grades
+    return gradesRounded
+print(roundGrade(inp))
+
